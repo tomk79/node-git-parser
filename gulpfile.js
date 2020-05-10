46757681ceac42f76dd52c567d0b45b81829a222
Tomoya Koyanagi <tomk79@gmail.com>
2020-05-10 22:03:51 +0900

    update gulp;

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,14 @@
-var gulp = require('gulp');
-var uglify = require("gulp-uglify");//JavaScriptファイルの圧縮ツール
-var concat = require('gulp-concat');//ファイルの結合ツール
-var plumber = require("gulp-plumber");//コンパイルエラーが起きても watch を抜けないようになる
-var rename = require("gulp-rename");//ファイル名の置き換えを行う
-var browserify = require("gulp-browserify");//NodeJSのコードをブラウザ向けコードに変換
-var packageJson = require(__dirname+'/package.json');
-var _tasks = [
-	'js'
-];
+let gulp = require('gulp');
+let uglify = require("gulp-uglify");//JavaScriptファイルの圧縮ツール
+let concat = require('gulp-concat');//ファイルの結合ツール
+let plumber = require("gulp-plumber");//コンパイルエラーが起きても watch を抜けないようになる
+let rename = require("gulp-rename");//ファイル名の置き換えを行う
+let browserify = require("gulp-browserify");//NodeJSのコードをブラウザ向けコードに変換
+let packageJson = require(__dirname+'/package.json');
 
 // JavaScript を処理
 gulp.task("js", function() {
-	gulp.src(["src/gitParse79.js"])
+	return gulp.src(["src/gitParse79.js"])
 		.pipe(plumber())
 		.pipe(browserify({}))
 		.pipe(concat('gitParse79.js'))
@@ -22,9 +19,13 @@ gulp.task("js", function() {
 	;
 });
 
+let _tasks = gulp.parallel(
+	'js'
+);
+
 // src 中のすべての拡張子を監視して処理
 gulp.task("watch", function() {
-	gulp.watch(["src/**/*","libs/**/*"], _tasks);
+	return gulp.watch(["src/**/*","libs/**/*"], _tasks);
 });
 
 // src 中のすべての拡張子を処理(default)
