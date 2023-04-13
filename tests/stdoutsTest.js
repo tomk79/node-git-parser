128f73f42f36e74fadc0251dbf5f4908b9053e7d
Tomoya Koyanagi <tomk79@gmail.com>
2023-04-14 00:07:06 +0900

    (add test pattern: git diff)

--- tests/stdoutsTest.js
+++ tests/stdoutsTest.js
@@ -3,7 +3,7 @@ const childProc = require('child_process');
 const fsEx = require('fs-extra');
 const GitParser = require('../libs/main.js');
 
-describe('git基本操作', function() {
+describe('バリエーション', function() {
 
 	it("git status", function(done) {
 		const gitParser = new GitParser(function(cmdAry, callback){
@@ -47,6 +47,30 @@ describe('git基本操作', function() {
 			assert.strictEqual(result.staged.modified.length, 2);
 			assert.strictEqual(result.isUnmerged, false);
 
+			done();
+
+		});
+	});
+
+	it("git diff -U12 hoge/fuga.txt", function(done) {
+		const gitParser = new GitParser(function(cmdAry, callback){
+			var stdout = fsEx.readFileSync(__dirname + '/stdouts/git-diff.txt');
+			callback(0, stdout.toString());
+			return;
+		});
+
+		this.timeout(60*1000);
+
+		gitParser.git(['diff', '-U12'], function(result){
+			console.log(result);
+			assert.equal(typeof(result), typeof({}));
+			assert.equal(typeof(result.stdout), typeof(''));
+			assert.strictEqual(result.diff.length, 1);
+			assert.strictEqual(result.diff[0].filenameBefore, 'testdata/test.jsx');
+			assert.strictEqual(result.diff[0].filename, 'testdata/test.jsx');
+			assert.strictEqual(result.diff[0].type, 'changed');
+			assert.strictEqual(result.diff[0].isRenamed, false);
+			assert.strictEqual(result.diff[0].similarity, false);
 			done();
 
 		});
